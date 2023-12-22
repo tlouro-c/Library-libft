74aed9893b62c78e075096339109317d3fc81432
tlouro-c <tomascorreia.lc@gmail.com>
2023-12-22 17:02:53 +0000

    22/12

---
+++ src/utils/ft_atoll.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlouro-c <tlouro-c@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 17:01:09 by tlouro-c          #+#    #+#             */
+/*   Updated: 2023/12/22 17:02:37 by tlouro-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long long	ft_atoll(const char *s)
+{
+	long long	n;
+	int			i;
+	int			negative;
+
+	n = 0;
+	i = 0;
+	while (ft_isspace(s[i]))
+		i++;
+	negative = s[i] == '-';
+	if (s[i] == '-' || s[i] == '+')
+		i++;
+	while (s[i] != '\0' && ft_isdigit(s[i]))
+		n = n * 10 + (s[i++] - '0');
+	if (negative)
+		return (n * -1);
+	return (n);
+}
